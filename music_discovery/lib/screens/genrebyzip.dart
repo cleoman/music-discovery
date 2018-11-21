b8a84c311c6e31aa58a9d606b5f8901bd5e9d4e1
irdamousa <eh1986@gmail.com>
2018-11-21 11:59:41 -0500

    Pages

--- music_discovery/lib/screens/genrebyzip.dart
+++ music_discovery/lib/screens/genrebyzip.dart
@@ -1,4 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_database/firebase_database.dart';
+
+class _Data {
+  String artist1, artist2, artist3;
+  String genre1, genre2, genre3;
+  String song1, song2, song3;
+  String zipcode;
+
+  _Data(this.artist1, this.artist2, this.artist3, this.genre1, this.genre2,
+      this.genre3, this.song1, this.song2, this.song3, this.zipcode);
+}
+
 
 
 class TrendingGenrePage extends StatefulWidget {
@@ -9,7 +21,11 @@ class TrendingGenrePage extends StatefulWidget {
   _TrendingGenreState createState() => _TrendingGenreState();
 }
 
+
 class _TrendingGenreState extends State<TrendingGenrePage> {
+  _Data query = new _Data('1','2','3','4','5','6','7','8','9','10');
+  var data;
+
   Widget build(BuildContext build) {
     return Scaffold(
         appBar: AppBar(
@@ -17,7 +33,36 @@ class _TrendingGenreState extends State<TrendingGenrePage> {
                 child: Text('Trending Genre Results')
             )
         ),
-        body: Text(widget.zipCode),
+        body: ListView(
+          children: <Widget>[
+            ListTile(
+              title: Text(query.genre1),
+            ),
+            ListTile(
+              title: Text(query.genre2),
+            ),
+            ListTile(
+              title: Text(query.genre3),
+            ),
+          ],
+        )//Text(widget.zipCode),
     );
   }
+
+  @override
+  void initState() {
+    super.initState();
+    DatabaseReference ref = FirebaseDatabase.instance.reference();
+    ref.child(widget.zipCode).once().then((DataSnapshot snap) {
+      this.setState(() {
+        data = snap.value;
+        query.artist1 = data["artist1"];
+        query.artist2 = data["artist2"];
+        query.artist3 = data["artist3"];
+      });
+
+    });
+
+  }
+
 }
--- music_discovery/lib/screens/songsbyzip.dart
+++ music_discovery/lib/screens/songsbyzip.dart
@@ -1,4 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_database/firebase_database.dart';
+
+class _Data {
+  String artist1, artist2, artist3;
+  String genre1, genre2, genre3;
+  String song1, song2, song3;
+  String zipcode;
+
+  _Data(this.artist1, this.artist2, this.artist3, this.genre1, this.genre2,
+      this.genre3, this.song1, this.song2, this.song3, this.zipcode);
+}
 
 
 class TrendingSongsPage extends StatefulWidget {
@@ -10,14 +21,29 @@ class TrendingSongsPage extends StatefulWidget {
 }
 
 class _TrendingSongsState extends State<TrendingSongsPage> {
+  _Data query = new _Data('1','2','3','4','5','6','7','8','9','10');
+  var data;
+
   Widget build(BuildContext build) {
     return Scaffold(
         appBar: AppBar(
-          title: Center(
-            child: Text('Trending Songs Results')
-          )
+            title: Center(
+                child: Text('Trending Songs Results')
+            )
         ),
-      body: Text(widget.zipCode),
+        body: ListView(
+          children: <Widget>[
+            ListTile(
+              title: Text(query.song1),
+            ),
+            ListTile(
+              title: Text(query.song2),
+            ),
+            ListTile(
+              title: Text(query.song3),
+            ),
+          ],
+        )//Text(widget.zipCode),
     );
   }
 }
